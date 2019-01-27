afd9d612cf63b8b02807c773b6c1fc3eafed3d72
Sebastian <sebastian@human-injection.de>
2019-01-27 18:42:07 +0100

    Default Playlist sort changed to Ascending

--- sources/webapp/scripts/globalUi.js
+++ sources/webapp/scripts/globalUi.js
@@ -373,7 +373,7 @@ var GlobalUserInterfaceHelper = {
     if (episodes && episodes.length > 0) {
       for (i = 0; i < episodes.length; i += 1) {
         entryUI = GlobalUserInterfaceHelper.renderEpisode(episodes[i])
-        if (order && order === 'asc') {
+        if (!order && order === 'asc') {
           listUI.append(entryUI)
         } else {
           listUI.prepend(entryUI)
